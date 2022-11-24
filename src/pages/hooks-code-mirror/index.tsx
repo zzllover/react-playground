a424f12c5670c25a2d011c1b44ffefd461cabf0e
😔 sad ming <ximing.peng@shopee.com>
2022-11-24 20:52:46 +0800

    fix: fix

--- src/pages/hooks-code-mirror/index.tsx
+++ src/pages/hooks-code-mirror/index.tsx
@@ -1,7 +1,11 @@
 import { useState } from "react";
 import { unifyDocs } from "./utils";
 import useCodeMirror from "./useCodeMirror";
-
+/**
+ * 
+ * demo from
+ * https://0xsuk.github.io/posts/2022-03-25-build-your-own-markdown-editor-with-react.js-and-codemirror-6/
+ */
 const HooksCodeMirror = () => {
   const [doc, setDoc] = useState("# hell1o12123123123");
   const {ref}= useCodeMirror({initialDoc: doc, setDoc})
